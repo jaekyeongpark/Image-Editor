d56bb99be3997238d04b2b046281eea141cc1527
jaekyeongpark <jaekyeong.dev@gmail.com>
2024-09-25 00:07:52 +0900

    [Modified] css 수정 / 원본 이미지로 돌아갈 수 있도록 reset 기능 추가

--- src/components/ImageEditor.jsx
+++ src/components/ImageEditor.jsx
@@ -1,9 +1,10 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import ImageProcessor from '@/utils/imageProcessor';
 
 const ImageEditor = ({ image, onEdit }) => {
   const canvasRef = useRef(null);
-  let processor = null;
+  const processorRef = useRef(null); // Use useRef to store the processor instance
+  const [originalImage, setOriginalImage] = useState(null);
 
   useEffect(() => {
     if (image && canvasRef.current) {
@@ -17,40 +18,60 @@ const ImageEditor = ({ image, onEdit }) => {
         canvas.width = img.width;
         canvas.height = img.height;
 
-        context.clearRect(0, 0, canvas.width, canvas.height); // 기존 그림 지우기
+        context.clearRect(0, 0, canvas.width, canvas.height);
         context.drawImage(img, 0, 0);
-      };
 
-      processor = new ImageProcessor(canvasRef.current);
+        const originalData = context.getImageData(0, 0, canvas.width, canvas.height);
+        setOriginalImage(originalData);
+
+        processorRef.current = new ImageProcessor(canvas);
+      };
 
       return () => URL.revokeObjectURL(img.src);
     }
   }, [image]);
 
-
   const grayScale = () => {
-    processor.applyGrayscale();
+    if (processorRef.current) {
+      processorRef.current.applyGrayscale();
+    }
   };
+
   const blackWhite = () => {
-    processor.applyBlackWhite();
+    if (processorRef.current) {
+      processorRef.current.applyBlackWhite();
+    }
   };
+
   const reverse = () => {
-    processor.applyReverse();
-  }
+    if (processorRef.current) {
+      processorRef.current.applyReverse();
+    }
+  };
+
+  const reset = () => {
+    if (originalImage && canvasRef.current) {
+      const canvas = canvasRef.current;
+      const context = canvas.getContext('2d');
+      context.putImageData(originalImage, 0, 0);
+    }
+  };
 
   return (
+    <>
+      <div className='edit-canvas-buttons'>
+        <button type="button" onClick={reset}>Reset</button>
+        <button type="button" onClick={grayScale}>Gray Scale</button>
+        <button type="button" onClick={blackWhite}>Black White</button>
+        <button type="button" onClick={reverse}>Reverse</button>
+      </div>
 
-    <div className='edit-canvas-wrapper'>
-      <button type="button" onClick={grayScale}>Gray Scale</button>
-      <button type="button" onClick={blackWhite}>Black White</button>
-      <button type="button" onClick={reverse}>Reverse</button>
-
-      {image ? (
-        <canvas ref={canvasRef} className='edit-canvas' />
-      ) : ""}
-
-
-    </div>
+      <div className='edit-canvas-wrapper'>
+        {image ? (
+          <canvas ref={canvasRef} className='edit-canvas' />
+        ) : ""}
+      </div>
+    </>
   );
 };
 
